08cff234766c103dedc63be68a8086ed69a8f0c8
gega99 <shotadzegega5@gmail.com>
2024-04-20 22:55:59 +0400

    commit

---
+++ day12/claswork/classwork.py
@@ -0,0 +1,15 @@
+a =int(input("enter a number"))
+b = int(input("enter a number"))
+for i in range(0,6):
+ print(i)
+
+ a= int(input("enter a number"))
+b = int(input("enter a number"))
+for i in range(6,0):
+ print(i)
+
+a= int(input("enter a number"))
+b =int(input("enter a number"))
+for i in range(2,4,6,8,10,12,14,16,18,20):
+ print(i)
+
---
+++ day12/homework/homework.py
@@ -0,0 +1,12 @@
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+for i in range(0,101 ):
+  print(i)
+
+  while i > 0:
+    print(i) 
+  i = i - 1
+
+
+for i in range(2,4,6,8,10,13,14,16,18,20,22,24,26,28,30):
+ print(i)
